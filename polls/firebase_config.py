1f8785de72bdd5fbec1e69e49c21d6b686113537
Valentina Rodriguez <vale.ro.sa@hotmail.com>
2025-05-03 22:37:36 -0500

    Actualiza settings y .gitignore, elimina archivos sensibles

--- polls/firebase_config.py
+++
@@ -1,9 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials, auth
-
-# Ruta del archivo JSON de Firebase (descárgalo desde Firebase Console)
-FIREBASE_CREDENTIALS_PATH = "C:/Users/danna/djangotutorial/proyecto-a4977-firebase-adminsdk-fbsvc-10a1288fd4.json"
-
-if not firebase_admin._apps:
-    cred = credentials.Certificate(FIREBASE_CREDENTIALS_PATH)
-    firebase_admin.initialize_app(cred)
